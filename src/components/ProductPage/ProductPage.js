0d51d20f4dd92a6dbb0a50f4de2652433aaa707d
RyanTan182 <kangkhen182@gmail.com>
2024-10-15 22:49:22 +1100

    added some thing sto Product Page

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -2,7 +2,10 @@ import React, { useEffect, useRef, useState } from 'react';
 import ProductCard from '../common/ProductCard';
 
 export default function ProductPage() { 
-    let product = [{ 'product_name': 'Piano Lessons at AUD$45/ph', 'seller_name': 'Megan Fox','reviews':'5', 'description': 'Hi! I can offer Piano lessons! AUD$45/ph' }]
+    let product = [{ 'product_name': 'Piano Lessons at AUD$45/ph', 
+        'seller_name': 'Megan Fox',
+        'reviews':'4', 
+        'description': 'At Megan Fox Piano Academy, we belive that anyone can master the piano with the right guidance and dedication. Whether you are a complete beginnner or looking to refind your skills, our personalised piano lessons are designed to help you achieve your musical goals at any stage of your journey.' }]
     return(
         <div>
             {product.map(d => <ProductCard data={d} />)}
