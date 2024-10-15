91d5a4710168b86aa01b3d5a488e249ac8c382c5
David Choulex <davidchoulex@Davids-MacBook-Air-3.local>
2024-10-15 16:38:00 +1100

    Install mui tailwindcss

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
 import ProductPage from './components/ProductPage/ProductPage';
 
-
 function App() {
   return (
       <Router>
         <div className="App">
         <Navbar />
           <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/products" element={<ProductPage />} />
+            <Route path="/" element={<WelcomePage />} />
+            <Route path="/products" element={<ProductPage />} />
           </Routes>
        </div>
       </Router>
-      
-      
-  
   );
 }
 
